4441fc3fe79cd4adb21cebb18d9ad564ebd9196e
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2019-01-16 11:01:41 -0600

    adds GraphQL query and renders homepage title and description.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,49 @@
-import React from "react"
+import React from "react";
+import { StaticQuery, graphql } from "gatsby";
 
-export default () => <div>Hello world!</div>
+const TitleAndDescription = ({ data }) => {
+  const title = data.site.siteMetadata.title;
+  const description = data.site.siteMetadata.description;
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        fontFamily: "avenir"
+      }}
+    >
+      <h2 style={{ marginBottom: 0 }}>{title}</h2>
+      <p style={{ marginTop: 0, opacity: 0.5 }}>{description}</p>
+    </div>
+  );
+};
+
+const Header = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+          }
+        }
+      }
+    `}
+    render={data => (
+      <div>
+        <TitleAndDescription data={data} />
+      </div>
+    )}
+  />
+);
+
+const Layout = () => (
+  <div>
+    <Header />
+  </div>
+);
+
+export default Layout;
