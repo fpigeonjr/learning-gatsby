b9ab95615f7784a80f219f4cecb84478106c31f7
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2019-01-17 16:17:12 -0600

    build page slugs

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
-import { graphql } from "gatsby";
+import { graphql, Link } from "gatsby";
 
 const Layout = ({ data }) => {
   const { edges } = data.allMarkdownRemark;
@@ -20,7 +20,7 @@ const Layout = ({ data }) => {
           const { frontmatter } = edge.node;
           return (
             <div key={frontmatter.path} style={{ marginBottom: "1rem" }}>
-              {frontmatter.title}
+              <Link to={frontmatter.path}>{frontmatter.title}</Link>
             </div>
           );
         })}
---
+++ src/templates/BlogPost.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { graphql } from "gatsby";
+
+const Template = props => {
+  return (
+    <div>
+      <h1>Blog Post Here</h1>
+    </div>
+  );
+};
+
+export default Template;
