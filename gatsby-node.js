8761e19ad0a71594fddf1bd5dd2873e8411f3c1a
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2019-01-18 13:12:52 -0600

    adds next and previous links to blog detail

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,17 +1,18 @@
-const path = require('path')
+const path = require("path");
 
-
-exports.createPages = (({graphql, actions}) => {
-  const { createPage } = actions
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
 
   return new Promise((resolve, reject) => {
-    const blogPostTemplate = path.resolve('src/templates/blogPost.js') 
+    const blogPostTemplate = path.resolve("src/templates/blogPost.js");
 
     resolve(
       graphql(
         `
           query {
-            allMarkdownRemark {
+            allMarkdownRemark(
+              sort: { order: ASC, fields: [frontmatter___date] }
+            ) {
               edges {
                 node {
                   frontmatter {
@@ -23,20 +24,23 @@ exports.createPages = (({graphql, actions}) => {
           }
         `
       ).then(result => {
+        const posts = result.data.allMarkdownRemark.edges;
 
-        result.data.allMarkdownRemark.edges.forEach(({node}) => {
-          const path = node.frontmatter.path
+        posts.forEach(({ node }, index) => {
+          const path = node.frontmatter.path;
           createPage({
             path,
             component: blogPostTemplate,
             context: {
-              pathSlug: path
+              pathSlug: path,
+              prev: index === 0 ? null : posts[index - 1].node,
+              next: index === posts.length - 1 ? null : posts[index + 1].node
             }
-          })
+          });
 
-          resolve()
-        })
+          resolve();
+        });
       })
-    )
-  })
-})
+    );
+  });
+};
--- src/templates/BlogPost.js
+++ src/templates/BlogPost.js
@@ -17,6 +17,12 @@ const Template = ({ data, pageContext }) => {
           fontFamily: "avenir"
         }}
       />
+      <div style={{ marginBottom: "1rem", fontFamily: "avenir" }}>
+        {next && <Link to={next.frontmatter.path}>Next Post</Link>}
+      </div>
+      <div style={{ fontFamily: "avenir" }}>
+        {prev && <Link to={prev.frontmatter.path}>Previous Post</Link>}
+      </div>
     </div>
   );
 };
